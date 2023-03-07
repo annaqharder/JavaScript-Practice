6522c93761ecf7008c5457c78771cdec371a7adf
Anna Harder <annaqharder@gmail.com>
2023-03-06 21:33:19 -0700

    TickingTime.js

---
+++ TickingTime.js
@@ -0,0 +1,13 @@
+// callback functions
+// setTimeout()
+
+function clock() {
+    const clockNode = document.querySelector("#clock")
+    return setInterval(() => {
+        let date = new Date();
+        let tick = date.toLocaleTimeString();
+        clockNode.textContent = tick
+    }, 1000)
+}
+
+clock();
