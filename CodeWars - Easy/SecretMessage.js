0ee842be2d2856115d132a434105fc1c17e0cc0f
Anna Harder <annaqharder@gmail.com>
2023-03-08 15:22:13 -0700

    isDivisible.js & MakeNegative.js

--- CodeWars - Easy/SecretMessage.js
+++ CodeWars - Easy/SecretMessage.js
@@ -2,7 +2,7 @@ function greet(name) {
     if (name === "Johnny") {
         return "Hello, my love!"
     } else {
-        return "Hello, " + name + "!";
+        return `Hello, ${name}!`;
     }
 }
 
