2ddc19ed3f15b0a2d0f6c9001406c2651bf6ac8c
Anna Harder <annaqharder@gmail.com>
2023-03-09 17:13:30 -0700

    alternative solution to MeanOfArray.js

--- CodeWars - Easy/MeanOfArray.js
+++ CodeWars - Easy/MeanOfArray.js
@@ -1,13 +1,17 @@
+// function getAverage(scoreArray) {
+//     const count = scoreArray.length;
+
+//     let sum = 0;
+
+//     for (let i = 0; i < count; i++) {
+//         sum = sum + scoreArray[i]
+//     }
+
+//     return Math.floor(sum / count)
+// }
+
 function getAverage(scoreArray) {
-    const count = scoreArray.length;
-
-    let sum = 0;
-
-    for (let i = 0; i < count; i++) {
-        sum = sum + scoreArray[i]
-    }
-
-    return Math.floor(sum / count)
+    return Math.floor(scoreArray.reduce((sum, x) => sum + x) / scoreArray.length);
 }
 
 console.log(getAverage([78, 90, 98, 97, 65, 88, 97]))
