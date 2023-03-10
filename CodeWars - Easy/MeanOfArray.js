58c4bbd65a094be21daadb2ee502ff721dbc42ea
Anna Harder <annaqharder@gmail.com>
2023-03-09 17:12:14 -0700

    finished MeanOfArray.js

--- CodeWars - Easy/MeanOfArray.js
+++ CodeWars - Easy/MeanOfArray.js
@@ -1,3 +1,13 @@
-function getAverage() {
-    .floor
+function getAverage(scoreArray) {
+    const count = scoreArray.length;
+
+    let sum = 0;
+
+    for (let i = 0; i < count; i++) {
+        sum = sum + scoreArray[i]
+    }
+
+    return Math.floor(sum / count)
 }
+
+console.log(getAverage([78, 90, 98, 97, 65, 88, 97]))
