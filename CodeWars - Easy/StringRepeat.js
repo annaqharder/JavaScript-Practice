e0437b4e87301de51f5e6acaa2033e60e10fc60b
Anna Harder <annaqharder@gmail.com>
2023-03-10 10:12:35 -0700

    alternatice StringRepeat.js

--- CodeWars - Easy/StringRepeat.js
+++ CodeWars - Easy/StringRepeat.js
@@ -1,10 +1,14 @@
-function repeatStr(numberOfRepeats, str) {
-    let starterString = '';
+// function repeatStr(numberOfRepeats, str) {
+//     let starterString = '';
 
-    for (let i = 0; i < numberOfRepeats; i++) {
-        starterString = starterString + str;
-    }
-    return starterString
+//     for (let i = 0; i < numberOfRepeats; i++) {
+//         starterString = starterString + str;
+//     }
+//     return starterString
+// }
+
+function repeatStr(n, s) {
+    return s.repeat(n)
 }
 
 console.log(repeatStr(5, "ha"))
