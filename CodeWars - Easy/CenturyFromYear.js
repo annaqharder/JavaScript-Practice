9c8748c6ddd6c2072e3bc96ded239ba814fae306
Anna Harder <annaqharder@gmail.com>
2023-03-11 22:24:01 -0700

    CenturyFromYear.js

---
+++ CodeWars - Easy/CenturyFromYear.js
@@ -0,0 +1,10 @@
+function century(year) {
+    return Math.ceil(year/100)
+}
+
+console.log(century(1901))
+console.log(century(1795))
+console.log(century(2023))
+console.log(century(1705))
+console.log(century(1601))
+console.log(century(2000))
