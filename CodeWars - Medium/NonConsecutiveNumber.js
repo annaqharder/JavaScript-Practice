8391b8476e03484952503d8b5507f63bf75c9a7d
Anna Harder <annaqharder@gmail.com>
2023-03-12 22:55:50 -0600

    NonConsecutiveNumber.js revised

--- CodeWars - Medium/NonConsecutiveNumber.js
+++ CodeWars - Medium/NonConsecutiveNumber.js
@@ -5,8 +5,11 @@ function FirstNonConsecutive(array) {
 
         if (curr + 1 !== next) {
             return next
-        }
+        } 
     }
+
+    return null
 }
 
 console.log(FirstNonConsecutive([1,2,3,4,6,7,8]))
+console.log(FirstNonConsecutive([1,2,3,4,5,6,7,8]))
