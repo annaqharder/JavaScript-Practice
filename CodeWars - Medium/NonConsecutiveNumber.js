a657998a570e34b501ce8cb65dc485fa9a458f69
Anna Harder <annaqharder@gmail.com>
2023-03-12 22:58:05 -0600

    NonConsecutiveNumber.js alternative solution

--- CodeWars - Medium/NonConsecutiveNumber.js
+++ CodeWars - Medium/NonConsecutiveNumber.js
@@ -1,14 +1,19 @@
+// function FirstNonConsecutive(array) {
+//     for(let i = 0; i < array.length; i++) {
+//         const curr = array[i];
+//         const next = array[i + 1]
+
+//         if (curr + 1 !== next) {
+//             return next
+//         }
+//     }
+//     return null
+// }
+
 function FirstNonConsecutive(array) {
-    for(let i = 0; i < array.length; i++) {
-        const curr = array[i];
-        const next = array[i + 1]
+    let result = array.find((val, index) => val !== index + array[0]);
 
-        if (curr + 1 !== next) {
-            return next
-        } 
-    }
-
-    return null
+    return (Number.isInteger(result)) ? result : null
 }
 
 console.log(FirstNonConsecutive([1,2,3,4,6,7,8]))
