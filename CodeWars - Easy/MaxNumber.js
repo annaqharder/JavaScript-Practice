74bbba4d4ec5ce2e2e6999ba54c733fc7e803be3
Anna Harder <annaqharder@gmail.com>
2023-03-15 14:11:03 -0600

    MaxNumber.js alternative solution

--- CodeWars - Easy/MaxNumber.js
+++ CodeWars - Easy/MaxNumber.js
@@ -1,23 +1,27 @@
+// function maxNumber(num) {
+//     // number to string
+//     const numToString= num.toString();
+
+//     // string to array
+//     const stringToArr = numToString.split('');
+
+//     // sort array from smallest to largest value
+//     const sortDescending = stringToArr.sort();
+
+//     // sort from ascending value
+//     const sortAscending = sortDescending.reverse();
+
+//     // array to string
+//     const arrToString = sortAscending.join('');
+
+//     // string to number
+//     const strToNum = Number(arrToString);
+
+//     return strToNum
+// }
+
 function maxNumber(num) {
-    // number to string
-    const numToString= num.toString();
-
-    // string to array
-    const stringToArr = numToString.split('');
-
-    // sort array from smallest to largest value
-    const sortDescending = stringToArr.sort();
-
-    // sort from ascending value
-    const sortAscending = sortDescending.reverse();
-
-    // array to string
-    const arrToString = sortAscending.join('');
-
-    // string to number
-    const strToNum = Number(arrToString);
-
-    return strToNum
+    return Number(num.toString().split('').sort((first, second) => second - first).join(''))
 }
 
 
