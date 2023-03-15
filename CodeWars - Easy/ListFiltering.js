57a1704c9ff6d0b7e6735eea7e2caaa1afbd4610
Anna Harder <annaqharder@gmail.com>
2023-03-14 22:07:24 -0600

    ListFiltering.js

--- CodeWars - Easy/ListFiltering.js
+++ CodeWars - Easy/ListFiltering.js
@@ -1,9 +1,16 @@
-function filter_list(list) {
-    return list.filter(())
+function filter_list(numArr) {
+    return numArr.filter(callbackFunc)
 }
 
 function callbackFunc(arrItem) {
-    if (typeof arrItem!== "number" || ) {
+   // 1. it is a number
+   // 2. is is positive
 
+    if (arrItem >= 0 && typeof arrItem === 'number') {
+        return true
+    } else {
+        return false
     }
 }
+
+console.log(filter_list([1, 2, 'a', 'b']))
