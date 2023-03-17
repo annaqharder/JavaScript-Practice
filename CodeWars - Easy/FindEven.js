c2c50cf7de4a3cfab0c4ee98af33cf574a35354a
Anna Harder <annaqharder@gmail.com>
2023-03-17 10:13:03 -0600

    FindEven.js alternative solution

--- CodeWars - Easy/FindEven.js
+++ CodeWars - Easy/FindEven.js
@@ -1,14 +1,19 @@
+// function findEven(arr) {
+
+//     const evenNumbers = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 === 0) {
+//             evenNumbers.push(arr[i])
+//         }
+//     }
+
+//     return evenNumbers
+// }
+
+
 function findEven(arr) {
-
-    const evenNumbers = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0) {
-            evenNumbers.push(arr[i])
-        }
-    }
-
-    return evenNumbers
+    return arr.filter((arr) => arr % 2 === 0)
 }
 
 console.log(findEven([1, 2, 3, 4, 5, 6]))
