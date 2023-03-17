3ead371fc720815b14a89e88d8f22094fa2565a4
Anna Harder <annaqharder@gmail.com>
2023-03-16 21:59:00 -0600

    MaxNumber.js comments added

--- CodeWars - Easy/MaxNumber.js
+++ CodeWars - Easy/MaxNumber.js
@@ -1,28 +1,31 @@
-// function maxNumber(num) {
-//     // number to string
-//     const numToString= num.toString();
+// my versin - yay!
+    // function maxNumber(num) {
+    //     // number to string
+    //     const numToString= num.toString();
 
-//     // string to array
-//     const stringToArr = numToString.split('');
+    //     // string to array
+    //     const stringToArr = numToString.split('');
 
-//     // sort array from smallest to largest value
-//     const sortDescending = stringToArr.sort();
+    //     // sort array from smallest to largest value
+    //     const sortDescending = stringToArr.sort();
 
-//     // sort from ascending value
-//     const sortAscending = sortDescending.reverse();
+    //     // sort from ascending value
+    //     const sortAscending = sortDescending.reverse();
 
-//     // array to string
-//     const arrToString = sortAscending.join('');
+    //     // array to string
+    //     const arrToString = sortAscending.join('');
 
-//     // string to number
-//     const strToNum = Number(arrToString);
+    //     // string to number
+    //     const strToNum = Number(arrToString);
 
-//     return strToNum
-// }
+    //     return strToNum
+    // }
 
-function maxNumber(num) {
-    return Number(num.toString().split('').sort((first, second) => second - first).join(''))
-}
+
+// condensed version
+    // function maxNumber(num) {
+    //     return Number(num.toString().split('').sort((first, second) => second - first).join(''))
+    // }
 
 
 console.log(maxNumber(213))
