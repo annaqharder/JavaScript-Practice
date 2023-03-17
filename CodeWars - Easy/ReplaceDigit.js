5c2ffa12ede3b2c26aabea563dc239abc1fde9d1
Anna Harder <annaqharder@gmail.com>
2023-03-17 14:46:57 -0600

    ReplaceDigit.js alternative solution

--- CodeWars - Easy/ReplaceDigit.js
+++ CodeWars - Easy/ReplaceDigit.js
@@ -2,4 +2,10 @@ function replaceDigit(str) {
     return str.replace(/[0-9]/, "$")
 }
 
+// function replaceDigit(str) {
+//     return str.replace(/\d/, "$")
+// }
+
 console.log(replaceDigit("abcd1234)"))
+
+// string.replace(searchValue, newValue)
