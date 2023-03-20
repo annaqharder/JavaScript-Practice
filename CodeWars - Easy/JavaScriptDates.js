bc8c4fd73e75b4d066b9f72512036eacc9fbd77a
Anna Harder <annaqharder@gmail.com>
2023-03-19 21:47:26 -0600

    JavaScriptDates.js

---
+++ CodeWars - Easy/JavaScriptDates.js
@@ -0,0 +1,9 @@
+const formatDate = (date = new Date()) => {
+        const days = date.getDate();
+        const months = date.getMonth() + 1;
+        const years = date.getFullYear();
+
+        return `${months}/${days}/${years}`
+}
+
+console.log(formatDate())
