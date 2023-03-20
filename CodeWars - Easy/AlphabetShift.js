0ae102176a8b4643b8ea19e82e48c99e2a377732
Anna Harder <annaqharder@gmail.com>
2023-03-19 21:42:10 -0600

    AlphabetShift.js attempt fringe situations

--- CodeWars - Easy/AlphabetShift.js
+++ CodeWars - Easy/AlphabetShift.js
@@ -1,5 +1,21 @@
+// function doesn't work when getting to the end of the alphabet
+
 function letterShift(str){
     return str.split('').map(char => String.fromCharCode(char.charCodeAt(0) + 1)).join('')
 }
 
-console.log(letterShift('abcd'))
+console.log(letterShift('z'))
+
+
+
+// function letterShift(str) {
+//     const array = str.split('');
+
+//     if (array === 'z') {
+//         return 'a'
+//     } else {
+//         return array.map(char => String.fromCharCode(char.charCodeAt(0) + 1)).join('')
+//     }
+// }
+
+// console.log(letterShift('z'))
