fce0a8d7079445802ca9e76d1b7d25809bf2c2f8
Anna Harder <annaqharder@gmail.com>
2023-03-22 17:28:47 -0600

    LeapYear.js

---
+++ CodeWars - Easy/LeapYear.js
@@ -0,0 +1,10 @@
+const isLeapYear = (year) => {
+    return year % 4 === 0
+}
+
+console.log(isLeapYear(2014))
+console.log(isLeapYear(2015))
+console.log(isLeapYear(2016))
+console.log(isLeapYear(2017))
+console.log(isLeapYear(2018))
+console.log(isLeapYear(2019))
