c652ee1d26c1c21ce7222ee7ea41f988373b7b67
Anna Harder <annaqharder@gmail.com>
2023-05-01 16:16:44 -0600

    alternative solution to 2DMatrix.js

--- LeetCode/2DMatrix.js
+++ LeetCode/2DMatrix.js
@@ -1,21 +1,41 @@
+// var searchMatrix = function(matrix, target) {
+
+//     if (!matrix.length) {
+//         return false
+//     }
+//     let i = 0;
+//     let j = matrix[0].length - 1;
+
+//     while(i < matrix.length && j >= 0) {
+//         if (matrix[i][j] > target) {
+//             j--
+//         } else if (matrix[i][j] < target) {
+//             i++
+//         } else {
+//             return true
+//         }
+//     }
+//     return false
+// }
+
+// console.log(searchMatrix([[1,3,5,7], [10,11,16,20], [23, 30, 34, 60]], 3))
+
 var searchMatrix = function(matrix, target) {
+    let rows = matrix.length;
+    let columns = matrix[0].length;
 
-    if (!matrix.length) {
-        return false
-    }
-    let i = 0;
-    let j = matrix[0].length - 1;
+    let left = 0;
+    let right = rows * columns - 1
 
-    while(i < matrix.length && j >= 0) {
-        if (matrix[i][j] > target) {
-            j--
-        } else if (matrix[i][j] < target) {
-            i++
-        } else {
-            return true
-        }
+    while(left <= right) {
+        let mid = Math.floor((left + right)/2);
+        let midVal = matrix[Math.floor(mid/columns)][mid%columns];
+
+        if(midVal === target) return true;
+        if(target < midVal) right = mid - 1;
+        else left = mid + 1;
     }
     return false
 }
 
-console.log(searchMatrix([[1,3,5,7], [10,11,16,20], [23, 30, 34, 60]], 3))
+console.log(searchMatrix([[1,3,5,7], [10,11,16,20], [23, 30, 34, 60]], 4))
