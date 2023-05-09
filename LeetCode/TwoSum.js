ef8c75f46dbc14c550abe0a578418693bd51bc87
Anna Harder <annaqharder@gmail.com>
2023-05-09 15:26:07 -0600

    TwoSums.js alternative solution

--- LeetCode/TwoSum.js
+++ LeetCode/TwoSum.js
@@ -1,11 +1,21 @@
-const twoSum = (array, target) => {
-    for ( let i = 0; i < array.length; i++) {
-         for ( let j= i++; i< array.length; j++) {
-              if ( array[j] === target - array[i]) return [i, j]               
-         };
-    };
-    return null;
-};
+// const twoSum = (array, target) => {
+//     for ( let i = 0; i < array.length; i++) {
+//          for ( let j= i++; i< array.length; j++) {
+//               if ( array[j] === target - array[i]) return [i, j]               
+//          };
+//     };
+//     return null;
+// };
+
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
 
 const arr1 = [3, 4, 5, 6, 7];
 const target1 = 8;
