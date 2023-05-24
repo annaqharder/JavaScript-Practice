7b421d64d095594ddfd230bb9c87555c9514a894
Anna Harder <annaqharder@gmail.com>
2023-05-24 14:55:56 -0600

    Code Challenge #3 more test data

--- Udemy/CodeChallenge3.js
+++ Udemy/CodeChallenge3.js
@@ -1,11 +1,11 @@
-const averageScoreDolphins = (97 + 112 + 101) / 3;
+const averageScoreDolphins = (97 + 130 + 101) / 3;
 const averageScoreKoalas = (109 + 95 + 123) / 3;
 
-if((averageScoreDolphins > averageScoreKoalas) && averageScoreDolphins > 100) {
+if((averageScoreDolphins > averageScoreKoalas) && averageScoreDolphins >= 100) {
     console.log("Dolphins are the winners")
 } else if ((averageScoreDolphins === averageScoreKoalas) && averageScoreDolphins >= 100 && averageScoreKoalas >= 100) {
     console.log("It's a tie!")
-} else if ((averageScoreKoalas > averageScoreDolphins) && averageScoreKoalas > 100){
+} else if ((averageScoreKoalas > averageScoreDolphins) && averageScoreKoalas >= 100){
     console.log("Koalas are the winners!")
 } else {
     console.log("No winner :(")
